aba6b1ef73470088b7de0dc9af0e8d8766e9b0f2
Marcin Dziedzic <contact@marcindziedzic.com>
2013-01-17 20:16:17 +0100

    temporarily exposing populate action

--- src/clojure/headteacher/views/workspace.clj
+++ src/clojure/headteacher/views/workspace.clj
@@ -2,7 +2,7 @@
   (:require [net.cgrand.enlive-html :as html])
   (:use [noir.core :only [defpage]]
         [headteacher.views.index :only [layout]]
-        [headteacher.datastore :only [get-sheets user]]))
+        [headteacher.datastore :only [get-sheets populate user]]))
 
 (html/defsnippet workspace "views/workspace.html" [:body :> html/any-node]
   [sheets]
@@ -13,3 +13,7 @@
 
 (defpage "/workspace" []
   (layout {:main (workspace (get-sheets user))}))
+
+(defpage "/populate" []
+  (populate)
+  "OK")
